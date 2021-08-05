be84a71dbb7d5cab94fff039d5c8cc7370b3254f
AbelChrist <abelimmanuelakristianto@gmail.com>
2021-08-05 13:00:43 +0700

    Fix responsive layout for mobile

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <h1>Tambah Siswa</h1>
     <div class="row">
-        <div class="col-8">
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
                     <h4>Form Tambah Siswa</h4>
